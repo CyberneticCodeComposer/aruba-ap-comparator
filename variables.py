0b2ffeb27c57b16b853c651181d7317700c55f79
CyberneticCodeComposer <140006118+CyberneticCodeComposer@users.noreply.github.com>
2023-07-19 13:49:51 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,8 @@
+# AOS 8 API Configuration
+aos8api = {
+    'username' : 'changeme',
+    'password' : 'changeme',
+    'base_url' : 'https://changeme:4343/v1',
+    'config_path' : 'configpath=/md/<cluster-group>/<controller MAC>',
+    'ap_group' : 'changeme'
+}
